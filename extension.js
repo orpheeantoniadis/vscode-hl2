fd98e0e29e30e86a0e21b7b0420b2b89f288202d
orphee.antoniad <orphee.antoniadis@hesge.ch>
2019-11-01 14:57:23 +0100

    Update python script so it can send a file with backspaces.

--- extension.js
+++ extension.js
@@ -2,8 +2,6 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const path = require('path');
-const readline = require('readline');
-const fs = require('fs');
 const spawn = require("child_process").spawn;
 
 // this method is called when your extension is activated
@@ -16,33 +14,19 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let connect_disposable = vscode.commands.registerCommand('extension.connect', function () {
-		var process = spawn('python',[path.join(__dirname, 'hl2-bridge.py')] ); 
-		process.stdout.on('data', function(data) { 
-			vscode.window.showInformationMessage(data.toString());
-		}) 
-	});
-
 	let upload_disposable = vscode.commands.registerCommand('extension.upload', function () {
 		let editor = vscode.window.activeTextEditor;
 		if (editor) {
 			let document = editor.document;
-			let readInterface = readline.createInterface({
-				input: fs.createReadStream(document.fileName),
-				output: process.stdout,
-				console: false
-			});
 			
-			readInterface.on('line', function(line) {
-				var process = spawn('python',[path.join(__dirname, 'hl2-bridge.py'), line + '\r\n'] ); 
-				process.stdout.on('data', function(data) { 
-					console.log(data.toString());
-				}) 
-			});
+			var process = spawn('python',[path.join(__dirname, 'hl2-bridge.py'), document.fileName] ); 
+			process.stdout.on('data', function(data) {
+				vscode.window.showInformationMessage(data.toString());
+			}) 
 		}
 	});
 
-	context.subscriptions.push(connect_disposable, upload_disposable);
+	context.subscriptions.push(upload_disposable);
 }
 exports.activate = activate;
 
--- hl2-bridge.py
+++ hl2-bridge.py
@@ -12,6 +12,11 @@ from serial.tools import list_ports
 VID = 0x1FC9
 PID = 0x0083
 
+INSTRUCTION_INTERVAL = 50
+EOL                  = '\r\n'
+BACKSPACE            = '\x08'
+BREAK                = '\x03'
+
 class HepiaLight2Communicator:
     def __init__(self):
         self.port = None
@@ -34,7 +39,7 @@ class HepiaLight2Communicator:
         self.usb.write(char.encode())
 
     def put_string(self, string):
-        com.usb.write(string.encode())
+        self.usb.write(string.encode())
         
     def put_int(self, int):
         self.usb.write(struct.pack('>I', int))
@@ -48,14 +53,50 @@ class HepiaLight2Communicator:
     def wait_char(self, char):
         while self.get_char() != char :
             pass
+
+class HepiaLight2Uploader(HepiaLight2Communicator):
+    def __init__(self, path):
+        super().__init__()
+        with open(path, 'rb') as fp :
+            self.lines = fp.readlines()
+
+    def send_line(self, line):
+        command = ''
+        indentation = 0
+
+        for i in range(len(line)):
+            if line[i] != ' ':
+                indentation = -1 if i % 2 else i
+                break
+
+        if indentation < self.last_indentation:
+            nb_backspace = int((self.last_indentation - indentation) / 2)
+            for i in range(nb_backspace):
+                command += BACKSPACE
+            if indentation == 0 and self.last_indentation > 0: 
+                command += EOL
+
+        self.last_indentation = indentation
+
+        command += line.strip()
+        command += EOL
+        self.put_string(command)
+
+    def send_file(self):
+        self.put_string(BREAK)
+        self.last_indentation = 0
+        for line in self.lines:
+            if (line and line != b'\r'):
+                self.send_line(line.decode())
+                time.sleep(INSTRUCTION_INTERVAL / 1000)
+        self.send_line('')
             
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
-        com = HepiaLight2Communicator()
+        print("Usage: python hl2-bridge.py <path>")
     else :
-        string = sys.argv[1]
-        com = HepiaLight2Communicator()
-        if com.usb != None:
-            com.put_string(string)
-    if com.usb != None:      
-        com.deinit()
+        path = sys.argv[1]
+        uploader = HepiaLight2Uploader(path)
+        if uploader.usb != None:
+            uploader.send_file()
+        uploader.deinit()
