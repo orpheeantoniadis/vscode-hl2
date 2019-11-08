b8063878447ad024abad54f43177791643772534
orphee.antoniad <orphee.antoniadis@hesge.ch>
2019-11-08 17:32:31 +0100

    Bump version to 0.3.0. - Restructure python scripts - Check the hepiaLight2 version befor updating - Complete .vscodeignore - Install pyserial on extension installation - Add an icon to the extension

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,43 @@ All notable changes to the "hl2" extension will be documented in this file.
 
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
-## [Unreleased]
+## [v0.3.0]
 
-- Initial release
+ * Restructure python scripts
+ * Check the hepiaLight2 version befor updating
+ * Complete .vscodeignore
+ * Install pyserial on extension installation
+ * Add an icon to the extension
+
+## [v0.2.1]
+
+ * Add a busy state to not be able to spam-click the buttons
+ * Show error messages instead of information when not connected or busy
+
+## [v0.2.0]
+
+ * Rename upload command to run
+ * Add an update command to update firmware to latest version
+ * Add run and update button to editor title
+ * Bug correction
+
+## [v0.1.2]
+
+ * Re-publish extension because of a problem using --yarn option
+
+## [v0.1.1]
+
+ * Add an update command to update firmware to latest version
+ * Add upload and update button to editor title
+ * Bug correction
+
+## [v0.1.0]
+
+ * Remove connect command
+ * Update python script to send files with indentation
+ * Bug correction
+
+
+## [v0.0.1]
+
+ * Initial release
--- src/extension.js
+++ src/extension.js
@@ -13,78 +13,78 @@ let script_running = false;
 let fimware_uptodate = false;
 
 function activate(context) {
-	let run_disposable = vscode.commands.registerCommand('extension.run', function () {
-		if (script_running) {
-			vscode.window.showErrorMessage('hepiaLight2 is busy');
-		} else {
-			let editor = vscode.window.activeTextEditor;
-			if (editor) {
-				let document = editor.document;
-				script_running = true;
-				PythonShell.run(find_script_path, {}, function(err, results) {
-					results.forEach(element => {
-						if (element == 'No hepiaLight2 connected') {
-							vscode.window.showErrorMessage(element);
-							script_running = false;
-						} else {
-							vscode.window.showInformationMessage(element);
-							vscode.window.showInformationMessage(`Running ${path.basename(document.fileName)} on hepiaLight2...`);
-							PythonShell.run(run_script_path, {args : [document.fileName]}, function(err, results) {
-								results.forEach(element => {
-									vscode.window.showInformationMessage(element);
-									script_running = false;
-								});
-							});
-						}
-					});
-				});
-			}
-		}
-	});
+    let run_disposable = vscode.commands.registerCommand('extension.run', function () {
+        if (script_running) {
+            vscode.window.showErrorMessage('hepiaLight2 is busy');
+        } else {
+            let editor = vscode.window.activeTextEditor;
+            if (editor) {
+                let document = editor.document;
+                script_running = true;
+                PythonShell.run(find_script_path, {}, function(err, results) {
+                    results.forEach(element => {
+                        if (element == 'No hepiaLight2 connected') {
+                            vscode.window.showErrorMessage(element);
+                            script_running = false;
+                        } else {
+                            vscode.window.showInformationMessage(element);
+                            vscode.window.showInformationMessage(`Running ${path.basename(document.fileName)} on hepiaLight2...`);
+                            PythonShell.run(run_script_path, {args : [document.fileName]}, function(err, results) {
+                                results.forEach(element => {
+                                    vscode.window.showInformationMessage(element);
+                                    script_running = false;
+                                });
+                            });
+                        }
+                    });
+                });
+            }
+        }
+    });
 
-	let update_disposable = vscode.commands.registerCommand('extension.update', function () {
-		if (script_running) {
-			vscode.window.showErrorMessage('hepiaLight2 is busy');
-		} else {
-			script_running = true;	
-			PythonShell.run(find_script_path, {}, function(err, results) {
-				results.forEach(element => {
-					if (element == 'No hepiaLight2 connected') {
-						vscode.window.showErrorMessage(element);
-						script_running = false;
-					} else {
-						vscode.window.showInformationMessage(element);
-						PythonShell.run(version_script_path, {}, function(err, results) {
-							results.forEach(element => {
-								fimware_uptodate = (element == firmware_version);
-								if (!fimware_uptodate) {
-									vscode.window.showInformationMessage(`Updating hepiaLight2 firmware to version ${firmware_version}...`);
-									PythonShell.run(program_script_path, {args : [firmware_path]}, function(err, results) {
-										results.forEach(element => {
-											vscode.window.showInformationMessage(element);
-											script_running = false;
-											fimware_uptodate = true;
-										});
-									});
-								} else {
-									vscode.window.showInformationMessage(`Firmware is up to date (version ${firmware_version})`);
-									script_running = false;
-								}
-							});
-						});
-					}
-				});
-			});
-		}
-	});
+    let update_disposable = vscode.commands.registerCommand('extension.update', function () {
+        if (script_running) {
+            vscode.window.showErrorMessage('hepiaLight2 is busy');
+        } else {
+            script_running = true;	
+            PythonShell.run(find_script_path, {}, function(err, results) {
+                results.forEach(element => {
+                    if (element == 'No hepiaLight2 connected') {
+                        vscode.window.showErrorMessage(element);
+                        script_running = false;
+                    } else {
+                        vscode.window.showInformationMessage(element);
+                        PythonShell.run(version_script_path, {}, function(err, results) {
+                            results.forEach(element => {
+                                fimware_uptodate = (element == firmware_version);
+                                if (!fimware_uptodate) {
+                                    vscode.window.showInformationMessage(`Updating hepiaLight2 firmware to version ${firmware_version}...`);
+                                    PythonShell.run(program_script_path, {args : [firmware_path]}, function(err, results) {
+                                        results.forEach(element => {
+                                            vscode.window.showInformationMessage(element);
+                                            script_running = false;
+                                            fimware_uptodate = true;
+                                        });
+                                    });
+                                } else {
+                                    vscode.window.showInformationMessage(`Firmware is up to date (version ${firmware_version})`);
+                                    script_running = false;
+                                }
+                            });
+                        });
+                    }
+                });
+            });
+        }
+    });
 
-	context.subscriptions.push(run_disposable, update_disposable);
+    context.subscriptions.push(run_disposable, update_disposable);
 }
 exports.activate = activate;
 
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
+    activate,
+    deactivate
 }
